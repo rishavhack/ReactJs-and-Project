f3216dd018ebe67d4fba2bba9d113794112fc626
Rishav <rishav.jnit@gmail.com>
2018-06-19 22:35:25 +0530

    Added  New Component using <Person/> or <Perosn></Perosn>

--- React Tutorial with Project/src/App.js
+++ React Tutorial with Project/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    /*return (
+    return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -13,9 +14,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Person></Person> 
       </div>
-    );*/
-    return React.createElement('div',{className:'App'},React.createElement('h1',null,'My name is Rishav'))
+    );
+   /// return React.createElement('div',{className:'App'},React.createElement('h1',null,'My name is Rishav'))
   }
 }
 
---
+++ React Tutorial with Project/src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const person = () => {
+	return <p>I am a Person !</p>
+};
+
+
+export default person;

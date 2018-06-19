b52117311e8faac4fdc5347478896f364d870af6
Rishav <rishav.jnit@gmail.com>
2018-06-20 05:16:45 +0530

    Passing argument ib Function For Dynamic Content

--- React Tutorial with Project/src/App.js
+++ React Tutorial with Project/src/App.js
@@ -14,9 +14,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Person></Person> 
-        <Person></Person> 
-        <Person></Person> 
+        <Person name='MAx' age="22"></Person> 
+        <Person name='Vinesh' age="24"></Person> 
+        <Person name='Diwakar' age="17"/>
       </div>
     );
    /// return React.createElement('div',{className:'App'},React.createElement('h1',null,'My name is Rishav'))

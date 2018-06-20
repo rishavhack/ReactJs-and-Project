97265ee3e7eb4414243b5212b0a8034195cf743f
Rishav <rishav.jnit@gmail.com>
2018-06-20 05:33:56 +0530

    Understanding & Using State

--- React Tutorial with Project/src/App.js
+++ React Tutorial with Project/src/App.js
@@ -4,6 +4,14 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+state = {
+  person:[
+    {name : 'Max',age :28},
+    {name : 'Gopal',age :23},
+    {name : 'Anish',age :25}
+  ]
+}
+
   render() {
     return (
       <div className="App">
@@ -14,9 +22,14 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Person name='MAx' age="22"></Person> 
+        <button>Switch Name</button>
+        <Person name='Max' age="22"></Person> 
         <Person name='Vinesh' age="24">My Hobbies : Racing</Person> 
         <Person name='Diwakar' age="17"/>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age}></Person> 
+        <Person name={this.state.person[1].name} age={this.state.person[1].age}>My Hobbies : Racing</Person> 
+        <Person name={this.state.person[2].name} age={this.state.person[2].sge}/>
+
       </div>
     );
    /// return React.createElement('div',{className:'App'},React.createElement('h1',null,'My name is Rishav'))

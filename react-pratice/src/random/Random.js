568139228c00cd9cd9a4515592d96ceb0a43afd5
Rishav <rishav.jnit@gmail.com>
2018-06-24 18:30:41 +0530

    Revision Completed of last week

--- react-pratice/src/random/Random.js
+++ react-pratice/src/random/Random.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import '../stateANDbutton/states.css';
 const random = (props) =>
 {
 	return (
-		<div>
-		<h4>Hi My name is {props.name} and age is {props.age}</h4>
+		<div className='stateStyle'>
+		<h4 onClick={props.change}>Hi My name is {props.name} and age is {props.age}</h4>
 		{props.children}
+		<input type="text" onChange={props.twobinding}/>
 		</div>
 		)
 }
--- react-pratice/src/stateANDbutton/stateANDbutton.js
+++ react-pratice/src/stateANDbutton/stateANDbutton.js
@@ -20,13 +20,38 @@ class StateButton extends Component
 		]
 		});
 	}
+	nameChanger = (newName) =>{
+		this.setState({
+			persons :[
+			{name:newName,age:24},
+			{name:'Hacker',age:38},
+		]
+		});
+	};
+	twoway = (event) =>{
+		this.setState({
+			persons :[
+			{name:'Love',age:24},
+			{name:event.target.value,age:38},
+		]
+		});
+	}
 	render()
 	{
+		const styling = {
+			backgroundColor:'White',
+	      font:'inherit',
+	      border:'1px solid blue',
+	      padding:'8px',
+	      cursor:'pointer'
+		};
 		return(
 				<div className="App">
-					<button onClick={this.switch}>Switch name</button>
-					<Random  name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-					<Random  name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+					<button onClick={this.switch} style={styling}>Switch name</button>
+					<button onClick={this.nameChanger.bind(this,'RishavSrivastava')} style={styling}>Switch name</button>
+					<button onClick={() =>this.nameChanger('RishuSrivastava')} style={styling}>Switch name</button>
+					<Random  name={this.state.persons[0].name} age={this.state.persons[0].age} change={this.nameChanger.bind(this,'Saumya')}/>
+					<Random  name={this.state.persons[1].name} age={this.state.persons[1].age} twobinding={this.twoway}/>
 				</div>
 				);
 	}

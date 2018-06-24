d4a30a5936e16c6bc5375f2a168de08b81e36da0
Rishav <rishav.jnit@gmail.com>
2018-06-24 18:08:22 +0530

    Add new css for Styling

--- React Tutorial with Project/src/Person/Person.js
+++ React Tutorial with Project/src/Person/Person.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './Person.css'
 const person = (props) => {
 	return (
-			<div>
+			<div className="Person"> 
 			<p onClick={props.click}>I am {props.name} and I am {props.age} and {Math.floor(Math.random() *30)} years old!!</p>
 			<p>{props.children}</p>
 			<input type='text'  onChange={props.change} />
